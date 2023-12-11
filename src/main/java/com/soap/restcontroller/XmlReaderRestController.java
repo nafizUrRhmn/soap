9484ee237d98042a0bac2ea5b46ff5aa4ea7ca6d
nafizUrRhmn <nafizurrahman.cse@gmail.com>
2023-12-11 17:40:41 +0600

    refactor: remove hashset

--- src/main/java/com/soap/restcontroller/XmlReaderRestController.java
+++ src/main/java/com/soap/restcontroller/XmlReaderRestController.java
@@ -1,38 +1,22 @@
 package com.soap.restcontroller;
 
-import com.soap.errorhandler.XmlErrorHandler;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.ResourceUtils;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
 
-
-import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Source;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.StringReader;
-import java.net.URL;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class XmlReaderRestController {
@@ -41,13 +25,13 @@ public class XmlReaderRestController {
     public ResponseEntity<?> processXml(@RequestBody String xml) throws SAXException, IOException {
         Document doc;
         try {
-            DocumentBuilder builder =  DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             doc = builder.parse(new InputSource(new StringReader(xml)));
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().body("Invalid XML format");
         }
-            // create a SchemaFactory capable of understanding WXS schemas
+        // create a SchemaFactory capable of understanding WXS schemas
 //            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 //            Source schemaFile = new StreamSource(ResourceUtils.getFile("classpath:PersonNotMatch.xsd"));
 //            Schema schema = factory.newSchema(schemaFile);
@@ -60,19 +44,16 @@ public class XmlReaderRestController {
 //            }
         doc.getDocumentElement().normalize();
         Map<String, String> map = new HashMap<>();
-        Set<Counter> countSet = new HashSet<>();
-        traverseXML(doc.getDocumentElement(), map, "", -1, null, countSet);
+        traverseXML(doc.getDocumentElement(), map, "", -1, null);
         for (Map.Entry<String, String> entry : map.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
-            return ResponseEntity.ok(map);
+        return ResponseEntity.ok(map);
 
     }
 
     private Integer traverseXML(Node node, Map<String, String> nodeMap, String parent,
-                                    Integer index, String currentParentNodeArray, Set<Counter> countSet) {
-//        int currentIndex = index;
-//        String currentParentNodeArray = currentParentNode;
+                                Integer index, String currentParentNodeArray) {
         if (node.getNodeType() == Node.ELEMENT_NODE) {
             String nodeName = node.getNodeName();
             String key = parent.isEmpty() ? nodeName : parent + "." + nodeName;
@@ -82,43 +63,43 @@ public class XmlReaderRestController {
                 for (int i = 0; i < nodeList.getLength(); i++) {
                     Node childNode = nodeList.item(i);
                     if (key.endsWith("LL")) {
-                        if(currentParentNodeArray!=null && currentParentNodeArray.equals(key)){
-                            int count = index+1;
-                            nodeMap.put(key+".size", count+"");
-                        }else{
-                            index =0;
+                        if (currentParentNodeArray != null && currentParentNodeArray.equals(key)) {
+                            int count = index + 1;
+                            nodeMap.put(key + ".size", count + "");
+                        } else {
+                            index = 0;
                         }
                         currentParentNodeArray = key;
                     }
-                    index = traverseXML(childNode, nodeMap, key, index, currentParentNodeArray, countSet);
+                    index = traverseXML(childNode, nodeMap, key, index, currentParentNodeArray);
                 }
             }
-        }else {
+        } else {
             String value = node.getTextContent().trim();
-            if(parent!= null && value!=null && value.length()>0){
+            if (parent != null && value != null && value.length() > 0) {
 
-                    String key;
-                    if(index>=0 && parent.contains(currentParentNodeArray)){
-                        System.out.println(index);
-                        int indexValue = parent.indexOf("LL") + 2;
-                        key = parent.substring(0, indexValue) + '['+index+']' + parent.substring(indexValue);
-                    }else {
-                        key = parent;
-                    }
-                    if(nodeMap.containsKey(key)){
-                        index++;
-                        int indexValue = parent.indexOf("LL") + 2;
-                        key = parent.substring(0, indexValue) + '['+index+']' + parent.substring(indexValue);
-                        System.out.println(key);
-                    }
-                    nodeMap.put(key, value);
+                String key;
+                if (index >= 0 && parent.contains(currentParentNodeArray)) {
+                    System.out.println(index);
+                    int indexValue = parent.indexOf("LL") + 2;
+                    key = parent.substring(0, indexValue) + '[' + index + ']' + parent.substring(indexValue);
+                } else {
+                    key = parent;
+                }
+                if (nodeMap.containsKey(key)) {
+                    index++;
+                    int indexValue = parent.indexOf("LL") + 2;
+                    key = parent.substring(0, indexValue) + '[' + index + ']' + parent.substring(indexValue);
+                    System.out.println(key);
+                }
+                nodeMap.put(key, value);
             }
         }
         return index;
     }
 
     @Data
-    private class Counter{
+    private class Counter {
         String name;
         Integer size;
     }
